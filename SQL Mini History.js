2ac3f479a80783a90b491802216d4641e39575fa
Michael Kovich <kovich@michaelkovich.com>
2018-03-06 16:57:11 -0600

    Steps 1, 2, 3, and 4 complete.

---
+++ SQL Mini History.js
@@ -0,0 +1,15 @@
+SELECT * from Artist;
+
+SELECT FirstName, LastName, Country
+FROM Employee;
+
+SELECT Name, Composer, Milliseconds
+FROM Track
+WHERE Milliseconds > 299000;
+
+SELECT count(*) 
+FROM Track 
+WHERE Milliseconds > 299000;
+
+// Black Diamond
+
